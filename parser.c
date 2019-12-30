6351a5b4361a007a72b819b10c819f63f340c4ce
capybara84 <eitoshi.ashihara@gmail.com>
2019-12-31 01:04:44 +0900

    add minimum decl

--- parser.c
+++ parser.c
@@ -1,5 +1,21 @@
 #include "mcc.h"
 
+#define COUNT_OF(array) (sizeof (array) / sizeof (array[0]))
+
+#ifdef NDEBUG
+#define next(pars)  ((pars)->token = next_token((pars)->scan))
+#else
+static TOKEN next(PARSER *pars)
+{
+    pars->token = next_token(pars->scan);
+    if (is_verbose_level(2)) {
+        printf("%s(%d): %s\n", pars->scan->filename, pars->scan->line,
+            scan_token_to_string(pars->scan, pars->token));
+    }
+    return pars->token;
+}
+#endif
+
 PARSER *open_parser_text(const char *filename, const char *text)
 {
     PARSER *pars = (PARSER*) alloc(sizeof (PARSER));
@@ -31,10 +47,138 @@ bool close_parser(PARSER *pars)
     return close_scanner(pars->scan);
 }
 
+static void parser_error(PARSER *pars, const char *s, ...)
+{
+    va_list ap;
+    va_start(ap, s);
+    verror(pars->scan->filename, pars->scan->line, s, ap);
+    va_end(ap);
+    exit(1);
+}
+
+static bool expect(PARSER *pars, TOKEN tk)
+{
+    if (pars->token == tk) {
+        next(pars);
+        return true;
+    }
+    parser_error(pars, "missing token %s", scan_token_to_string(pars->scan, tk));
+    return false;
+}
+
+
+static bool is_token_begin_with(PARSER *pars, TOKEN array[], int count)
+{
+    int i;
+    for (i = 0; i < count; i++)
+        if (pars->token == array[i])
+            return true;
+    return false;
+}
+
+static bool is_declaration_specifier(PARSER *pars)
+{
+    static TOKEN begin_with[] = { TK_STATIC, TK_EXTERN,
+            TK_VOID, TK_INT, };
+    return is_token_begin_with(pars, begin_with, COUNT_OF(begin_with));
+}
+
+
+/*
+compound_statement
+	= '{' '}'
+*/
+static void parse_compound_statement(PARSER *pars)
+{
+
+}
+
+/*
+declarator
+	= {'*'} (IDENTIFIER | '(' declarator ')') ['(' ')']
+*/
+static void parse_declarator(PARSER *pars)
+{
+    while (pars->token == TK_STAR) {
+        next(pars);
+    }
+    if (pars->token == TK_ID) {
+        next(pars);
+    } else if (pars->token == TK_LPAR) {
+        next(pars);
+        parse_declarator(pars);
+        expect(pars, TK_RPAR);
+    } else
+        parser_error(pars, "syntax error");
+    if (pars->token == TK_LPAR) {
+        next(pars);
+        expect(pars, TK_RPAR);
+    }
+}
+
+/*
+declaration_specifier
+	= storage_class_specifier | type_specifier
+storage_class_specifier
+    = STATIC | EXTERN
+type_specifier
+	= VOID | INT
+*/
+static void parse_declaration_specifier(PARSER *pars)
+{
+    switch (pars->token) {
+    case TK_STATIC:
+    case TK_EXTERN:
+    case TK_VOID:
+    case TK_INT:
+        next(pars);
+        break;
+    default:
+        parser_error(pars, "syntax error");
+    }
+}
+
+
+/*
+declaration_specifiers
+    = declaration_specifier {declaration_specifier}
+*/
+static void parse_declaration_specifiers(PARSER *pars)
+{
+    parse_declaration_specifier(pars);
+    while (is_declaration_specifier(pars)) {
+        parse_declaration_specifier(pars);
+    }
+}
+
+/*
+external_declaration
+    = declaration_specifiers declarator ';'
+    | declaration_specifiers declarator compound_statement
+*/
+static void parse_external_delaration(PARSER *pars)
+{
+    parse_declaration_specifiers(pars);
+    parse_declarator(pars);
+    if (pars->token == TK_SEMI) {
+        next(pars);
+    } else if (pars->token == TK_BEGIN) {
+        parse_compound_statement(pars);
+    } else
+        parser_error(pars, "syntax error");
+}
+
+/*
+translation_unit
+    = {external_declaration}
+*/
 NODE *parse(PARSER *pars)
 {
     NODE *np = NULL;
 
+    while (pars->token != TK_EOF) {
+        parse_external_delaration(pars);
+    }
     return np;
 }
 

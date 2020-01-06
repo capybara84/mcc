bc55e9d1daf98ae0507b6acd768d8177ca3996a9
capybara84 <eitoshi.ashihara@gmail.com>
2020-01-06 22:08:16 +0900

    fix test_parser

---
+++ test_parser1.c
@@ -0,0 +1,32 @@
+int a;
+void foo();
+int bar() {}
+static int b;
+extern void baz();
+int *p;
+int **pp;
+void *ptr;
+int (*pfn)();
+int (**ppfn)();
+static int **spp;
+
+int fun() {
+    int a, b;
+    a = 123;
+    if (a != 0)
+        b = 1;
+    else
+        b = 2;
+    while (a == 1)
+        a = a - 1;
+}
+
+int f() {
+    int a, b;
+    a = 123;
+    b = 0;
+    for (a = 0; a < 10; a = a + 1)
+        b = b + a;
+    return b;
+}
+

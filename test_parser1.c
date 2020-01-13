ac2af6354afa26ce384a16593c7e61fb0ac87558
capybara84 <eitoshi.ashihara@gmail.com>
2020-01-13 22:53:29 +0900

    mod test_parser1

--- test_parser1.c
+++ test_parser1.c
@@ -1,3 +1,35 @@
+/*
+external_declaration
+    = declaration_specifiers declarator ';'
+    | declaration_specifiers declarator compound_statement
+
+declaration_specifiers
+    = declaration_specifier {declaration_specifier}
+
+declaration_specifier
+	= storage_class_specifier | type_specifier
+
+storage_class_specifier
+    = STATIC | EXTERN
+
+type_specifier
+	= VOID | INT
+
+declarator
+	= {'*'} (IDENTIFIER | '(' declarator ')') ['(' [parameter_list] ')']
+
+abstract_declarator
+    =  '*' {'*'}
+    = {'*'} '(' abstract_declarator ')' ['(' [parameter_list] ')']
+
+parameter_list
+	= parameter_declaration {',' parameter_declaration}
+
+parameter_declaration
+	= declaration_specifiers declarator
+    | declaration_specifiers [abstract_declarator]
+
+*/
 int a;
 void foo();
 int bar() {}
@@ -10,22 +42,4 @@ int (*pfn)();
 int (**ppfn)();
 static int **spp;
 
-int fun() {
-    int a, b;
-    a = 123;
-    if (a != 0)
-        b = 1;
-    else
-        b = 2;
-    while (a == 1)
-        a = a - 1;
-}
-
-int f() {
-    int a, b;
-    a = 123;
-    b = 0;
-    for (a = 0; a < 10; a = a + 1)
-        b = b + a;
-    return b;
-}
+void f() {}
